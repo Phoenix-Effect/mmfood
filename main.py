117a93ab78ba3a52e8fc405cee2e625f08fe896f
Suhail Ghafoor <suhail.ghafoor@asu.edu>
2022-09-05 22:42:12 -0700

    fixed formatting, removed comments

--- main.py
+++ main.py
@@ -16,31 +16,6 @@ from flask_frozen import Freezer
 from flask_flatpages import FlatPages
 import sys
 
-# In this case, we will load templates off the filesystem.
-# This means we must construct a FileSystemLoader object.
-#
-# The search path can be used to make finding templates by
-#   relative paths much easier.  In this case, we are using
-#   absolute paths and thus set it to the filesystem root.
-# templateLoader = jinja2.FileSystemLoader( searchpath= os.path.join(os.getcwd(), "templates/" ))
-
-# # An environment provides the data necessary to read and
-# #   parse our templates.  We pass in the loader object here.
-# templateEnv = jinja2.Environment( loader=templateLoader )
-
-# # This constant string specifies the template file we will use.
-# TEMPLATE_FILE = "home.jinja"
-
-# # Read the template file using the environment object.
-# # This also constructs our Template object.
-# template = templateEnv.get_template( TEMPLATE_FILE )
-
-# # Specify any input variables to the template as a dictionary.
-# templateVars = { "title" : "Test Example",
-#                  "description" : "A simple inquiry of function." }
-
-# # Finally, process the template to produce our final text.
-# outputText = template.render( templateVars )
 
 app = Flask(__name__)
 app.config.from_object(__name__)
