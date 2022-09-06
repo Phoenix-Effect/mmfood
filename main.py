0c4ae54217473f567a767d8c03f59ac90ce53a77
Suhail Ghafoor <suhail.ghafoor@asu.edu>
2022-09-05 22:16:48 -0700

    install flask script

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def import_or_install(package):
     except ImportError:
         pip.main(['install', package])    
 
+import_or_install('flask')
 
 import json
 from flask import Flask, render_template
